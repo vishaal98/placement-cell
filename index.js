8ca8833255c2c0936a3b82c9a04ca017df85be65
Vishaal <vishnair001@gmail.com>
2023-11-12 16:20:56 +0530

    set up home page with controller route and view

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 PORT = 3000;
 const db = require("./config/mongoose");
 
+require("dotenv").config();
+const path = require("path");
+
+const assetsPath = path.join(__dirname, "assets");
+app.use("/assets", express.static(assetsPath));
+
+// setting up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", require("./routes"));
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log("Error starting the server: ", err);
